b90f68a3d17428e30005e30a88c0a754698d18f1
ahlem <achouchene@jasmineconseil.com>
2023-04-08 12:42:02 +0200

    upgrade to 16.0.0-next.7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,70 +1,95 @@
-import { Component, signal, computed , effect,Effect, Signal } from '@angular/core';
+import {
+  Component,
+  signal,
+  computed,
+  Signal,
+  EffectRef,
+  effect,
+  Injector,
+  Inject,
+  inject,
+  EnvironmentInjector,
+  runInInjectionContext,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 import { MatButtonModule } from '@angular/material/button';
 
 export interface Movie {
   name: string;
   genre: string;
   releaseYear: number;
-  upVote: number
+  upVote: number;
 }
 
-
-
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: true,
-    imports: [MatIconModule, RouterOutlet, MatButtonModule],
-    animations: [
-      trigger('slideInOut', [
-        state('in', style({
-          'max-height': '1000px', 'opacity': '1', 'visibility': 'visible'
-        })),
-        state('out', style({
-          'max-height': '0px', 'opacity': '0', 'visibility': 'hidden'
-        })),
-        transition('in => out', [animate('400ms ease-in-out')]),
-        transition('out => in', [animate('400ms ease-in-out')])
-      ])
-    ]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [MatIconModule, RouterOutlet, MatButtonModule],
+  animations: [
+    trigger('slideInOut', [
+      state(
+        'in',
+        style({
+          'max-height': '1000px',
+          opacity: '1',
+          visibility: 'visible',
+        })
+      ),
+      state(
+        'out',
+        style({
+          'max-height': '0px',
+          opacity: '0',
+          visibility: 'hidden',
+        })
+      ),
+      transition('in => out', [animate('400ms ease-in-out')]),
+      transition('out => in', [animate('400ms ease-in-out')]),
+    ]),
+  ],
 })
 export class AppComponent {
-
-  movieSig = signal<Movie|null>(null);
-  newMovieSig = computed( () => {
-          let newMovie = {name : "Titanic",genre : "Romance",releaseYear: 1997, upVote:this.movieSig()?.upVote};
-          return newMovie;
-  }
-);
-  effectSig! :Effect;
+  movieSig = signal<Movie | null>(null);
+  injector = inject(EnvironmentInjector);
+  newMovieSig = computed(() => {
+    let newMovie = {
+      name: 'Titanic',
+      genre: 'Romance',
+      releaseYear: 1997,
+      upVote: this.movieSig()?.upVote,
+    };
+    return newMovie;
+  });
+  effectSig!: EffectRef;
   title = 'angular-signals';
 
-
-
-  setMovie(){
-    this.movieSig.set(
-      {
-        name: 'Spider-Man',
-        genre: 'Action, Aventure',
-        releaseYear: 2002,
-        upVote: 8
-      }
-    )
+  setMovie() {
+    this.movieSig.set({
+      name: 'Spider-Man',
+      genre: 'Action, Aventure',
+      releaseYear: 2002,
+      upVote: 8,
+    });
   }
 
-  updateMovie(){
+  updateMovie() {
     this.movieSig.update((movie) => {
-      if(movie)
-        movie.upVote = movie.upVote+1;
+      if (movie) movie.upVote = movie.upVote + 1;
       return movie;
-    })
+    });
   }
 
-  mutateMovie(){
+  mutateMovie() {
     this.movieSig.mutate((movie) => {
       if (movie) {
         movie.upVote = 2000;
@@ -72,18 +97,20 @@ export class AppComponent {
     });
   }
 
-  createEffect(){
-    this.effectSig = effect( () => {
-        alert (`side effect angular signal after movie changes ${JSON.stringify(this.movieSig())}`)
-    }
-    )
-
+  createEffect() {
+    runInInjectionContext(this.injector,
+      () =>
+        (this.effectSig = effect(() => {
+          alert(
+            `side effect angular signal after movie changes ${JSON.stringify(
+              this.movieSig()
+            )}`
+          );
+        }))
+    );
   }
 
-  destroyEffect(){
+  destroyEffect() {
     this.effectSig?.destroy();
-
   }
-
-
 }
